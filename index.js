45b9250ee49eca0c69588b064d8e68f7892aefbe
Dirksen <dirksen@brandfire.ca>
2017-05-03 20:17:32 -0400

    Upgrade koa-ractive to be Koa@2 compatible

--- index.js
+++ index.js
@@ -5,9 +5,41 @@ var fs = require('fs')
 
 rcu.init(Ractive)
 
+/**
+ * Shallow copy two objects into a new object
+ *
+ * Objects are merged from left to right. Thus, properties in objects further
+ * to the right are preferred over those on the left.
+ *
+ * @param {object} obj1
+ * @param {object} obj2
+ * @returns {object}
+ * @api private
+ */
+
+function merge (obj1, obj2) {
+	var c = {},
+		keys = Object.keys(obj2),
+		i;
+
+	for (i = 0; i !== keys.length; i++) {
+		c[keys[i]] = obj2[keys[i]];
+	}
+
+	keys = Object.keys(obj1);
+	for (i = 0; i !== keys.length; i++) {
+		if (!c.hasOwnProperty(keys[i])) {
+			c[keys[i]] = obj1[keys[i]];
+		}
+	}
+
+	return c;
+};
+
+
 module.exports = function (directory, opts) {
 	var base=directory
-	
+
 	function parser(path) {
 		console.log(path)
 		var template = { v:1, t:[] }
@@ -18,10 +50,11 @@ module.exports = function (directory, opts) {
 	}
 
 	function render(path, data) {
-		console.log('render:', base, path, data)
 		var url = base+'/'+path+'.ract'
-		console.log(url)
-		
+
+		// Merge context locals with data
+		data = merge(this.locals || {}, data || {})
+
 		var html = new Ractive({
 			template: parser(url),
 			data: data
@@ -29,8 +62,8 @@ module.exports = function (directory, opts) {
 		this.body = html;
 	}
 
-	return function *(next) {
-		this.render = render
-		yield* next
+	return async (ctx, next) => {
+		ctx.render = render
+		await next()
 	}
 }
